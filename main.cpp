db8329525a351e0218a0b8285a4b15df3d1ff990
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air.local>
2025-12-19 07:49:51 -0800

    the number counting

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-# include <iostream>
+#include <iostream>
+using namespace std;
+#include "Student.h"
+//#include "Node.h"
+
+int main() {
+  int count;
+  cout<< "How many students do you want to enter:";
+  cin>> count;
+
+
+  for (int i =0; i<count; i++) {
+    char name [50];
+  }
+  return 0;
+}
